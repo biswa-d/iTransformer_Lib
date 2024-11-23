22cf735474f0b871d53f61ff28274f09e026f278
biswa-d <dsign.biswa@gmail.com>
2024-11-23 14:25:23 -0500

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,7 +2,7 @@
 
 # Set hyperparameters for training
 MODEL_ID="custom_model"
-MODEL="Reformer" #test
+MODEL="iTransformer" #test
 DATA="custom"
 ROOT_PATH="./data/"
 TRAIN_DATA="itransformer_train.csv"
