77483211bd72ef859e7001a0d78d9d697a6ded6b
biswa-d <dsign.biswa@gmail.com>
2024-11-24 16:08:30 -0500

    updated run_lstm

--- run_lstm.sh
+++ run_lstm.sh
@@ -2,11 +2,11 @@
 
 # Set hyperparameters for training
 MODEL_ID="custom_model"
-MODEL="LSTM"  # Change model to LSTM for comparison
+MODEL="LSTM" #test
 DATA="custom"
 ROOT_PATH="./data/"
-TRAIN_DATA="itransformer_train.csv"
-TEST_DATA="itransformer_test.csv"
+TRAIN_DATA="lg_train.csv"
+TEST_DATA="lg_valid.csv"
 FEATURES="MS"
 TARGET="Voltage"
 SEQ_LEN=400
@@ -15,11 +15,19 @@ PRED_LEN=1
 ENC_IN=3
 DEC_IN=3
 C_OUT=1
+D_MODEL=128
+N_HEADS=4
+E_LAYERS=2
+D_LAYERS=1
+D_FF=1024
+MOVING_AVG=25
+FACTOR=1
 DEVICES="0,1"
 TRAIN_EPOCHS=5000
 BATCH_SIZE=400
 PATIENCE=100
-LEARNING_RATE=0.0001
+LEARNING_RATE=0.001
+DROPOUT=0.2
 
 # Train the model
 echo "Starting training on GPUs $DEVICES..."
@@ -37,11 +45,19 @@ python run.py --is_training 1 \
                --enc_in $ENC_IN \
                --dec_in $DEC_IN \
                --c_out $C_OUT \
+               --d_model $D_MODEL \
+               --n_heads $N_HEADS \
+               --e_layers $E_LAYERS \
+               --d_layers $D_LAYERS \
+               --d_ff $D_FF \
+               --moving_avg $MOVING_AVG \
+               --factor $FACTOR \
                --devices $DEVICES \
                --train_epochs $TRAIN_EPOCHS \
                --batch_size $BATCH_SIZE \
                --patience $PATIENCE \
-               --learning_rate $LEARNING_RATE
+               --learning_rate $LEARNING_RATE \
+                --dropout $DROPOUT
 
 # Test the model
 echo "Starting testing on GPUs $DEVICES..."
@@ -50,7 +66,7 @@ python run.py --is_training 0 \
                --model $MODEL \
                --data $DATA \
                --root_path $ROOT_PATH \
-               --data_path $TEST_DATA \
+               --data_path $TRAIN_DATA \
                --features $FEATURES \
                --target $TARGET \
                --seq_len $SEQ_LEN \
@@ -59,4 +75,16 @@ python run.py --is_training 0 \
                --enc_in $ENC_IN \
                --dec_in $DEC_IN \
                --c_out $C_OUT \
-               --devices $DEVICES
+               --d_model $D_MODEL \
+               --n_heads $N_HEADS \
+               --e_layers $E_LAYERS \
+               --d_layers $D_LAYERS \
+               --d_ff $D_FF \
+               --moving_avg $MOVING_AVG \
+               --factor $FACTOR \
+               --devices $DEVICES \
+               --train_epochs $TRAIN_EPOCHS \
+               --batch_size $BATCH_SIZE \
+               --patience $PATIENCE \
+               --learning_rate $LEARNING_RATE \
+                --dropout $DROPOUT
