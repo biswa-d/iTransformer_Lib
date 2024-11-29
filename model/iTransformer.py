8f76c1da0f2970126f4d26293bd84e2efb79197d
biswa-d <dsign.biswa@gmail.com>
2024-11-29 18:08:59 -0500

    udpate

--- model/iTransformer.py
+++ model/iTransformer.py
@@ -75,7 +75,7 @@ class Model(nn.Module):
         )
         self.projector = nn.Sequential(
             nn.Linear(configs.d_model, configs.pred_len * 5, bias=True),  # Project to 5-step predictions
-            nn.Permute(0, 2, 1),  # Rearrange to (B, 5, N) for Conv1d
+            nn.permute(0, 2, 1),  # Rearrange to (B, 5, N) for Conv1d
             nn.Conv1d(
                 in_channels=5,
                 out_channels=1,  # Reduce to 1 prediction per sequence
