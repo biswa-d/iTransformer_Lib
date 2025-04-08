64b38c1a2e57685c2bd3caddefaa7ae6c8c3c354
biswa-d <dsign.biswa@gmail.com>
2025-04-08 10:05:06 -0500

    WIP: local changes to run_cv1.sh

--- run_cv1.sh
+++ run_cv1.sh
@@ -41,7 +41,7 @@ LR_WARMUP_EPOCHS=5 # Set to 0 to disable
 
 # <<< Custom Multi-Phase Schedule Params >>>
 MAIN_DECAY_EPOCHS=100 # Number of epochs for initial cosine decay after warmup (Set > 0 to enable custom)
-EXPLOIT_LR=0.0002     # Starting LR for exploitation cycles (defaults to min_lr if None/empty)
+EXPLOIT_LR=0.0001     # Starting LR for exploitation cycles (defaults to min_lr if None/empty)
 EXPLOIT_CYCLE_EPOCHS=20 # Length of each exploitation cycle
 
 # --- Learning Rate Schedule Option (Set ONE block) ---
